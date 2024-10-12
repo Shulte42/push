69f3645fe701c1702e265e95e0773fcfa7c371f9
Shulte42 <146744333+Shulte42@users.noreply.github.com>
2024-10-12 12:42:54 +0100

    Add files via upload

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,11 +6,36 @@
 /*   By: bruda-si <bruda-si@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:17:56 by bruda-si          #+#    #+#             */
-/*   Updated: 2024/10/08 11:52:43 by bruda-si         ###   ########.fr       */
+/*   Updated: 2024/10/12 12:36:06 by bruda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+void	handle_arguments(int ac, char **av, t_node **a)
+{
+	char	**split_av;
+	int		i;
+
+	split_av = NULL;
+	if (ac == 2)
+	{
+		split_av = ft_split(av[1], ' ');
+		init_stack_a(a, split_av, ac == 2);
+	}
+	else
+		init_stack_a(a, av + 1, ac == 2);
+	if (ac == 2 && split_av)
+	{
+		i = 0;
+		while (split_av[i])
+		{
+			free(split_av[i]);
+			i++;
+		}
+		free(split_av);
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -21,13 +46,7 @@ int	main(int ac, char **av)
 	b = NULL;
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
-	else if (ac == 2)
-	{
-		av = ft_split(av[1], ' ');
-		init_stack_a(&a, av, ac == 2);
-	}
-	else
-		init_stack_a(&a, av + 1, ac == 2);
+	handle_arguments(ac, av, &a);
 	if (!check_if_is_sorted(a))
 	{
 		if (stack_length(a) == 2)
--- src/push_swap/stack_init.c
+++ src/push_swap/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: bruda-si <bruda-si@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:14:30 by bruda-si          #+#    #+#             */
-/*   Updated: 2024/10/09 17:09:05 by bruda-si         ###   ########.fr       */
+/*   Updated: 2024/10/12 12:19:49 by bruda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ void	init_stack_a(t_node **a, char **av, bool argc_2)
 	int		i;
 
 	i = 0;
+	if (!(av[i]))
+		free_errors(a, av, argc_2);
 	while (av[i])
 	{
 		if (syntax_error(av[i]))
@@ -78,29 +80,6 @@ void	init_stack_a(t_node **a, char **av, bool argc_2)
 			free_errors(a, av, argc_2);
 		append_node(a, (int)number);
 		i++;
-	}
-}
-
-void	prep_for_push(t_node **stack,
-						t_node *top_node,
-						char stack_name)
-{
-	while (*stack != top_node)
-	{
-		if (stack_name == 'a')
-		{
-			if (top_node->above_median)
-				ra(stack, false);
-			else
-				rra(stack, false);
-		}
-		else if (stack_name == 'b')
-		{
-			if (top_node->above_median)
-				rb(stack, false);
-			else
-				rrb(stack, false);
-		}
 	}
 }
 
